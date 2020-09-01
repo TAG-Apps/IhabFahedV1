1c2c9bdbee8e6e6c3ace8b9ce882bb9638514cc2
FaHED <fahed_roos@yahoo.com>
2020-09-01 16:58:45 +0300

    Ihab v1

---
+++ app/src/main/java/com/tag/myapplication/IhabChikenActivity.java
@@ -0,0 +1,14 @@
+package com.tag.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IhabChikenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ihab_chiken);
+    }
+}
